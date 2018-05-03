b0aff7664172134b70d74981da87d277b9814231
Włodzimierz Pavlov <pavlovcoder@gmail.com>
2018-05-03 22:22:26 +0200

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -11,15 +11,21 @@ import Contact from './Contact';
 class Main extends Component {
   render() {
     return (
-      <div>
-        <h1>Simple SPA</h1>
-        <ul className='header'>
-          <li><a href='/'>Home</a></li>
-          <li><a href='/stuff'>Stuff</a></li>
-          <li><a href='/contact'>Contact</a></li>
-        </ul>
-        <div className='content'></div>
-      </div>
+      <HashRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className='header'>
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className='content'>
+            <Route exact path="/" component={Home}/>
+            <Route path="/stuff" component={Stuff}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
